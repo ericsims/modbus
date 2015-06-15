6691b025a8724a65fdbc0f680c642fae6205fe92
Eric Sims <EricSimsM@gmail.com>
2015-06-15 16:31:21 -0400

    reading from sql database

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('powerdata.db', sqlite3.OPEN_READONLY, function(err){
+    if(err) { 
+      console.log(err);
+      process.exit(1);
+    }
+  });
 var express = require('express');
 var app = express();
+
+var polldb = setInterval(function() { 
+  db.each("SELECT * FROM powerdata", function(err, data) {
+    if(err) console.log(err);
+    console.log('data: ' + JSON.stringify(data));
+  });
+}, 1000);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
